3dcdf11327d3f8337e765ec889cc4d94e41d72f5
nimishjindal <nimish.jindal@yahoo.com>
2022-11-29 03:09:25 +0530

    faster 2225. Find Players With Zero or One Losses

--- src/Players_With_0_or_1_Losses_2225/Solution.java
+++ src/Players_With_0_or_1_Losses_2225/Solution.java
@@ -5,29 +5,32 @@ import java.util.*;
 public class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
         int[] losses = new int[100001];
-        Set<Integer> players = new HashSet<>();
+        for (int i = 0; i < 100001; i++)
+            losses[i] = -1;
 
         for (int[] match : matches) {
             int winner = match[0];
             int loser = match[1];
-            players.add(winner);
-            players.add(loser);
 
-            losses[loser]++;
+            if (losses[winner] == -1)
+                losses[winner] = 0;
+
+            if (losses[loser] == -1)
+                losses[loser] = 1;
+            else
+                losses[loser]++;
+
         }
 
         List<Integer> noLoss = new ArrayList<>();
         List<Integer> oneLoss = new ArrayList<>();
 
-        players
-                .stream()
-                .sorted()
-                .forEach(player -> {
-                    if (losses[player] == 0)
-                        noLoss.add(player);
-                    if (losses[player] == 1)
-                        oneLoss.add(player);
-                });
+        for (int player = 0; player < 100001; player++) {
+            if (losses[player] == 0)
+                noLoss.add(player);
+            if (losses[player] == 1)
+                oneLoss.add(player);
+        }
         return Arrays.asList(noLoss, oneLoss);
     }
 }
