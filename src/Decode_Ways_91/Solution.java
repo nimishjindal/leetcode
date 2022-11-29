cd8fe2e8af74a65be1e9251f09aa51b28be5fbcf
nimishjindal <nimish.jindal@yahoo.com>
2022-11-29 14:35:12 +0530

    faster 91. Decode Ways

--- src/Decode_Ways_91/Solution.java
+++ src/Decode_Ways_91/Solution.java
@@ -1,31 +1,37 @@
 package Decode_Ways_91;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     int size;
     char[] str;
 
+    Map<Integer, Integer> dp;
+
     public int numDecodings(String s) {
         str = s.toCharArray();
         size = s.length();
+        dp = new HashMap<>();
+        dp.put(size, 1);
 
-        return build(0, str[0] + "", false);
+        return build(0);
     }
 
-    private int build(int i, String curr, boolean combined) {
-        if (i >= size || !isValid(curr))
+    private int build(int i) {
+        if (dp.containsKey(i))
+            return dp.get(i);
+        if (str[i] == '0')
             return 0;
-        if (i == size - 1)
-            return 1;
 
-        int count = build(i + 1, "" + str[i + 1], false);
-        if (!combined)
-            count += build(i + 1, str[i] + "" + str[i + 1], true);
-
+        int count = build(i + 1);
+        if (i + 1 < size && isValid(str[i], str[i + 1]))
+            count += build(i + 2);
+        dp.put(i, count);
         return count;
     }
 
-    private boolean isValid(String val) {
-        int num = Integer.parseInt(val);
-        return num >= 1 && num <= 26;
+    private boolean isValid(char a, char b) {
+        return (a == '1') || (a == '2' && '0' <= b && b <= '6');
     }
 }
