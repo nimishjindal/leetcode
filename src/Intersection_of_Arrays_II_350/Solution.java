afe3e7b87bb2d5e56e9be258aa97fd0ca6a0730f
nimishjindal <nimish.jindal@yahoo.com>
2022-12-10 00:18:29 +0530

    another way 350. Intersection of Two Arrays II

--- src/Intersection_of_Arrays_II_350/Solution.java
+++ src/Intersection_of_Arrays_II_350/Solution.java
@@ -4,37 +4,25 @@ import java.util.*;
 
 public class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map1 = buildFreq(nums1);
-        Map<Integer, Integer> map2 = buildFreq(nums2);
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
 
-        Map<Integer, Integer> intersection = new HashMap<>();
-        int size = 0;
-
-        for (int it : map2.keySet()) {
-            if (map1.containsKey(it)) {
-                int freq = Math.min(map2.get(it), map1.get(it));
-                size += freq;
-                intersection.put(it, freq);
+        List<Integer> list = new ArrayList<>();
+        int i = 0, j = 0, n = nums1.length, m = nums2.length;
+        while (i<n && j<m){
+            if (nums1[i] == nums2[j]){
+                list.add(nums1[i]);
+                i++;
+                j++;
             }
+            else if (nums1[i] < nums2[j])
+                i++;
+            else
+                j++;
         }
-
-        int i = 0;
-        int[] res = new int[size];
-        for (Map.Entry<Integer, Integer> entry : intersection.entrySet()) {
-            int num = entry.getKey();
-            int freq = entry.getValue();
-            while (freq-- > 0) res[i++] = num;
-        }
-
-        return res;
-    }
-
-    private Map<Integer, Integer> buildFreq(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int a : nums) {
-            int f = map.getOrDefault(a, 0) + 1;
-            map.put(a, f);
-        }
-        return map;
+        int[] ans = new int[list.size()];
+        int x=0;
+        for(int a : list) ans[x++] = a;
+        return ans;
     }
 }
--- src/Sample.java
+++ src/Sample.java
@@ -1,12 +1,12 @@
-import Merge_Sorted_Array_88.Solution;
+import Intersection_of_Arrays_II_350.Solution;
 
 public class Sample {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] arr = {1};
-        s.merge(
-                new int[]{1,2,3,0,0,0}, 3,
-                new int[]{2,5,6}, 3
+        s.intersect(
+                new int[]{1, 2, 3, 3, 3, 5},
+                new int[]{1, 2, 3, 5, 6}
         );
     }
 }
