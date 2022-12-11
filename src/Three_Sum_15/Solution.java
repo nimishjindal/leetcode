8c345e18914161e06fa2d433679a76d622d3971b
nimishjindal <nimish.jindal@yahoo.com>
2022-12-12 02:16:05 +0530

    faster 15. 3Sum

--- src/Three_Sum_15/Solution.java
+++ src/Three_Sum_15/Solution.java
@@ -1,37 +1,40 @@
 package Three_Sum_15;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Solution {
 
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> ans = new HashSet<>();
+        Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet<>();
         Integer prev = null;
         for (int i = 0; i < nums.length; i++) {
             if (prev == null || prev != nums[i])
-                ans.addAll(twoSum(nums, i, -1 * nums[i]));
+                set.addAll(twoSum(nums, i, -1 * nums[i]));
             prev = nums[i];
         }
-        return new ArrayList<>(ans);
+        return new ArrayList<>(set);
     }
 
     private Set<List<Integer>> twoSum(int[] nums, int start, int target) {
-        Set<Integer> set = new HashSet<>();
-        int a = nums[start];
-        Set<List<Integer>> ans = new HashSet<>();
-        for (int i = start + 1; i < nums.length; i++) {
-            int cur = nums[i];
-            int req = target - cur;
-            if (set.contains(req))
-                ans.add(Stream
-                        .of(a, cur, req)
-                        .sorted()
-                        .collect(Collectors.toList())
-                );
-            set.add(cur);
+        int end = nums.length - 1;
+        int a = nums[start++];
+        Set<List<Integer>> set = new HashSet<>();
+        while (start < end) {
+            int b = nums[start];
+            int c = nums[end];
+            int sum = b + c;
+            if (sum == target) {
+                start++;
+                end--;
+                set.add(Arrays.asList(a, b, c));
+            } else if (sum < target)
+                start++;
+            else
+                end--;
+            if (b == c)
+                break;
         }
-        return ans;
+        return set;
     }
 }
