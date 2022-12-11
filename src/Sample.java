8992ebbe6f8d61de7da82801f7f3f762e46a165f
nimishjindal <nimish.jindal@yahoo.com>
2022-12-11 22:26:28 +0530

    169. Majority Element

--- src/Sample.java
+++ src/Sample.java
@@ -1,10 +1,10 @@
-import Buy_Sell_Stock_121.Solution;
+import Majority_Element_169.Solution;
 
 public class Sample {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] arr = {1};
-        s.maxProfit(new int[]{7,1,5,3,6,4});
+        System.out.println(s.majorityElement(new int[]{2,2,1,1,1,2,2}));
         //s.maxProfit(new int[]{1,4,2});
     }
 }
