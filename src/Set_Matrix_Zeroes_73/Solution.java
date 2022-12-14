b0efde9e118bbf15f25bff3f32ae0d3ca81d556a
nimishjindal <nimish.jindal@yahoo.com>
2022-12-14 21:40:54 +0530

    faster 73. Set Matrix Zeroes

--- src/Set_Matrix_Zeroes_73/Solution.java
+++ src/Set_Matrix_Zeroes_73/Solution.java
@@ -1,49 +1,23 @@
 package Set_Matrix_Zeroes_73;
 
 public class Solution {
-    private int m, n;
-    private boolean[][] visited;
 
     public void setZeroes(int[][] matrix) {
-        m = matrix.length;
-        n = matrix[0].length;
-        this.visited = new boolean[m][n];
+        int n = matrix.length;
+        int m = matrix[0].length;
+        boolean[] isZeroCol = new boolean[n];
+        boolean[] isZeroRow = new boolean[m];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
                 if (matrix[i][j] == 0) {
-                    fillRow(i);
-                    break;
+                    isZeroCol[i] = true;
+                    isZeroRow[j] = true;
                 }
-            }
-        }
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < m; i++) {
-                if (matrix[i][j] == 0) {
-                    fillCol(j);
-                    break;
-                }
-            }
-        }
 
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < m; i++) {
-                if (this.visited[i][j]) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                if (isZeroCol[i] || isZeroRow[j])
                     matrix[i][j] = 0;
-                }
-            }
-        }
-    }
-
-    private void fillCol(int j) {
-        for (int i = 0; i < m; i++) {
-            visited[i][j] = true;
-        }
-    }
-
-    private void fillRow(int i) {
-        for (int j = 0; j < n; j++) {
-            visited[i][j] = true;
-        }
     }
 }
