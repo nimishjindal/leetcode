bb93463dca30a45b8167919cac1443a49ccf6b2b
nimishjindal <nimish.jindal@yahoo.com>
2022-12-23 23:51:40 +0530

    tabulation 198. House Robber

--- src/House_Robber_198/Solution.java
+++ src/House_Robber_198/Solution.java
@@ -1,26 +1,18 @@
 package House_Robber_198;
 
-import java.util.Arrays;
-
 public class Solution {
-    private int[] dp;
 
     public int rob(int[] nums) {
         int n = nums.length;
-        dp = new int[n];
-        Arrays.fill(dp, -1);
-        return rob(n - 1, nums);
-    }
-
-    private int rob(int pos, int[] nums) {
-        if (pos == 0)
-            return nums[0];
-        if (pos < 0)
-            return 0;
-        if (dp[pos] != -1)
-            return dp[pos];
-        int take = nums[pos] + rob(pos - 2, nums);
-        int notTake = rob(pos - 1, nums);
-        return dp[pos] = Math.max(take, notTake);
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for (int pos = 1; pos < n; pos++) {
+            int take = nums[pos];
+            if (pos - 2 >= 0)
+                take += dp[pos - 2];
+            int notTake = dp[pos - 1];
+            dp[pos] = Math.max(take, notTake);
+        }
+        return dp[n - 1];
     }
 }
