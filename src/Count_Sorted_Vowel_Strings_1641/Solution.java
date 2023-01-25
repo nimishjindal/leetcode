c37f9a40d949a4b87d1eae2125c2359cd2b6b326
nimishjindal <nimish.jindal@yahoo.com>
2023-01-26 00:23:27 +0530

    faster 1641. Count Sorted Vowel Strings

--- src/Count_Sorted_Vowel_Strings_1641/Solution.java
+++ src/Count_Sorted_Vowel_Strings_1641/Solution.java
@@ -1,18 +1,21 @@
 package Count_Sorted_Vowel_Strings_1641;
 
+import java.util.Arrays;
+
 public class Solution {
     public int countVowelStrings(int n) {
-        return countVowelStrings(0, n, '0');
-    }
-
-    public int countVowelStrings(int i, int n, char prev) {
-        if (i == n)
-            return 1;
-        int ans = 0;
-        for (char ch : "aeiou".toCharArray()) {
-            if (i == 0 || prev <= ch)
-                ans += countVowelStrings(i + 1, n, ch);
+        int[] prev = new int[5];
+        Arrays.fill(prev, 1);
+        int sum = 5;
+        for (int i = 1; i < n; i++) {
+            int[] dp = new int[5];
+            dp[0] = sum;
+            for (int j = 1; j < 5; j++) {
+                dp[j] = dp[j - 1] - prev[j - 1];
+                sum += dp[j];
+            }
+            prev = dp;
         }
-        return ans;
+        return sum;
     }
 }
--- src/Sample.java
+++ src/Sample.java
@@ -3,6 +3,7 @@ import Count_Sorted_Vowel_Strings_1641.Solution;
 public class Sample {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.countVowelStrings(2));
+        for (int i=1;i<=6;i++)
+            System.out.println(s.countVowelStrings(i));
     }
 }
