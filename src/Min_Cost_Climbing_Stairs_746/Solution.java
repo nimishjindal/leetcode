af6f33767b8b059a8f284b7d9174e75670ae0bcc
nimishjindal <nimish.jindal@yahoo.com>
2023-01-25 21:59:47 +0530

    space opt 746. Min Cost Climbing Stairs

--- src/Min_Cost_Climbing_Stairs_746/Solution.java
+++ src/Min_Cost_Climbing_Stairs_746/Solution.java
@@ -3,15 +3,16 @@ package Min_Cost_Climbing_Stairs_746;
 public class Solution {
 
     public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length + 1];
-
-        dp[1] = dp[0] = 0;
+        int curr, prev, prevprev;
+        curr = prev = prevprev = 0;
 
         for (int i = 2; i <= cost.length; i++) {
-            int left = cost[i - 1] + dp[i - 1];
-            int right = cost[i - 2] + dp[i - 2];
-            dp[i] = Math.min(left, right);
+            int left = cost[i - 1] + prev;
+            int right = cost[i - 2] + prevprev;
+            curr = Math.min(left, right);
+            prevprev = prev;
+            prev = curr;
         }
-        return dp[cost.length];
+        return curr;
     }
 }
