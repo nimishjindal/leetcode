cd64b1dd2425ef5b0cedf1313f8a5187e938539f
nimishjindal <nimish.jindal@yahoo.com>
2023-01-25 21:55:48 +0530

    faster 746. Min Cost Climbing Stairs

--- src/Min_Cost_Climbing_Stairs_746/Solution.java
+++ src/Min_Cost_Climbing_Stairs_746/Solution.java
@@ -1,23 +1,17 @@
 package Min_Cost_Climbing_Stairs_746;
 
-import java.util.Arrays;
-
 public class Solution {
+
     public int minCostClimbingStairs(int[] cost) {
         int[] dp = new int[cost.length + 1];
-        Arrays.fill(dp, -1);
-        return minCostClimbingStairs(cost.length, cost, dp);
-    }
 
-    public int minCostClimbingStairs(int i, int[] cost, int[] dp) {
-        if (dp[i] != -1)
-            return dp[i];
-        int left = 0;
-        int right = 0;
-        if (i >= 1)
-            left = cost[i - 1] + minCostClimbingStairs(i - 1, cost, dp);
-        if (i >= 2)
-            right = cost[i - 2] + minCostClimbingStairs(i - 2, cost, dp);
-        return dp[i] = Math.min(left, right);
+        dp[1] = dp[0] = 0;
+
+        for (int i = 2; i <= cost.length; i++) {
+            int left = cost[i - 1] + dp[i - 1];
+            int right = cost[i - 2] + dp[i - 2];
+            dp[i] = Math.min(left, right);
+        }
+        return dp[cost.length];
     }
 }
